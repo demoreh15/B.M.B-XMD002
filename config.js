556452d429fcf7bb42d1c4c4656f241f17b4749e
demoreh15 <demoreh15@gmail.com>
2025-05-06 15:03:42 +0300

    .

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "B.m.b-Xmd~IHMg2Ajb#DTVlDLR18ejXP7Vw4n4lHDLa9BLKiYaiRffxPkUlg4U",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
